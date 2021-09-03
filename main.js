9f2fb141e0f1b06675c6dbeb4b04d5f10cbaa102
ronnapatp <ronnapatp@users.noreply.github.com>
2021-09-03 18:57:27 +0700

    Link ch to main

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 // import file
 const en = require('./badword/en')
 const th = require('./badword/th')
-
+const ch = require('./badword/ch')
 
 client.on('ready', () => {
     // send message in terminal when run bot
@@ -16,6 +16,7 @@ client.on('ready', () => {
     // run file from badword folder
   en(client)
   th(client)
+  ch(client)
 });
 
 // login
