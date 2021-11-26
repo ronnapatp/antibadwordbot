5c4a98dbf2a0fe6406e4f92258f2c5d7ad330ae4
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-11-26 17:35:58 +0700

    Update comment

--- command/filter.js
+++ command/filter.js
@@ -1,29 +1,19 @@
-// Export function
 module.exports = (client) => {
-  // Handle on message is sent
   client.on('messageCreate', async (message) => {
-    // Lowercase the message
     const msg = message.content.toLowerCase();
 
-    // Get full conutry name from country code
     const country = new Intl.DisplayNames(['en'], { type: 'region' });
 
-    // Import badWords.js from ../badWords
     const badWords = require('../badWords/badWords');
 
-    // Loop all badWords
     for (const words of badWords) {
-      // Loop all content
       for (let i = 0; i < words.fileContent.length; i++) {
-        // If message has bad words
         if (words.fileContent[i].includes(msg)) {
-          // Delete message
           await message.delete();
+          // You can add action that bot do to people that uing badword ex. message.author.send("Stop use badword")
 
-          // Log message
           console.log(message.author.username, 'is using a badword');
 
-          // Send message not to rude
           return await message.channel.send(
             `Badword removed! ${country.of(
               words.fileName.toUpperCase()
--- main.js
+++ main.js
@@ -2,37 +2,25 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const fs = require('fs');
-
-// Create a new Discord client
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
-
-// Handle when bot is ready
 client.on('ready', () => {
-  // Bot's invitation link
   const { Permissions } = require('discord.js');
   const link = client.generateInvite({
     permissions: [
-      // You can add permissions here
       Permissions.FLAGS.ADMINISTRATOR,
     ],
     scopes: ['bot'],
   });
-  // Log to console when bot is ready
   console.log(`Logged in as ${client.user.tag}!\nInvite link : ${link}`);
 
-  // Set bot's presence
   client.user.setActivity('that who use badword!', { type: 'WATCHING' });
 
-  // Read all files in the commands folder
   const commandFiles = fs
     .readdirSync('./command')
     .filter((file) => file.endsWith('.js'));
-  // Loop through all files in the badWords directory
   for (const file of commandFiles) {
     require(`./command/${file}`)(client);
   }
 });
 
-// Login to Discord with your app's token
-// Find your bot's token here - https://discordapp.com/developers/applications/me
 client.login(process.env.DISCORD_TOKEN);
